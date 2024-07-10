ed21994451393161b826053f6b19d9e8f67cb00f
marufoi22 <m.y.882soccer@gmail.com>
2024-07-11 00:46:45 +0900

    method visibility

--- curriculum/php/VendingMachine/VendingMachine.php
+++ curriculum/php/VendingMachine/VendingMachine.php
@@ -2,23 +2,42 @@
 
 class VendingMachine
 {
+    private int $inputCoin = 0;
+    const CIDER_PRICE = 100;
+
     public function __construct(private string $manufacturerName)
     {
-
     }
 
     public function pressButton()
     {
-        return 'cider';
+        if($this->inputCoin >= self::CIDER_PRICE){
+            $this->inputCoin - self::CIDER_PRICE;
+            return 'cider' . PHP_EOL;
+        }
     }
 
-    public function pressManufacturerName()
+    private function pressManufacturerName()
     {
-        return $this->manufacturerName;
+        return $this->manufacturerName . PHP_EOL;
     }
 
+    public function depositCoin(int $coin)
+    {
+        if($coin % 100 === 0)
+        {
+            $this->inputCoin += $coin;
+        }
+    }
 }
 
 $vendingMachine = new VendingMachine('サントリー');
-echo $vendingMachine->pressButton() . PHP_EOL;
-echo $vendingMachine->pressManufacturerName() . PHP_EOL;
+echo $vendingMachine->pressButton();
+
+$vendingMachine->depositCoin(150);
+echo $vendingMachine->pressButton();
+
+$vendingMachine->depositCoin(100);
+echo $vendingMachine->pressButton();
+
+echo $vendingMachine->pressManufacturerName();
