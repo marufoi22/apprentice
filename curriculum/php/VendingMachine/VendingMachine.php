4216ac2c484add4d5c30b6f276fa901abee81f60
marufoi22 <m.y.882soccer@gmail.com>
2024-07-11 00:29:42 +0900

    add constructor, pressManufacturerName function

--- curriculum/php/VendingMachine/VendingMachine.php
+++ curriculum/php/VendingMachine/VendingMachine.php
@@ -2,12 +2,23 @@
 
 class VendingMachine
 {
+    public function __construct(private string $manufacturerName)
+    {
+
+    }
+
     public function pressButton()
     {
         echo 'cider' . PHP_EOL;
     }
 
+    public function pressManufacturerName()
+    {
+        return $this->manufacturerName;
+    }
+
 }
 
-$vendingMachine = new VendingMachine();
+$vendingMachine = new VendingMachine('サントリー');
 echo $vendingMachine->pressButton();
+echo $vendingMachine->pressManufacturerName() . PHP_EOL;
